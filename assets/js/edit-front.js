fbd962b50ac03f5ad47f3722d8a892d0af5cb284
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-11-28 00:18:46 +0900

    list 에서 선택한 user의 정보를 수정 form에서 출력

--- assets/js/edit-front.js
+++ assets/js/edit-front.js
@@ -1,68 +1,30 @@
-
-//화면에 in뭐냐 
+//화면에 in뭐냐
 
 getUsers()
-
 
 //첫 화면
 
 let user = null
-//go=> nil  java=> null 이건 비어있어서 들어가는 데이터타입이 곧 변수가 됨 
+//go=> nil  java=> null 이건 비어있어서 들어가는 데이터타입이 곧 변수가 됨
 async function getUsers() {
     const a = location.search // ?id=1
     // a.slice(1) //id=1  => 이렇게 하려면 a = a.slice(1) 이렇게 다시 해줘야 함
-    //이거 안해도 되는데? 굳이 하는 이유는? 안해도 됨. 그저예시였음. 
+    //이거 안해도 되는데? 굳이 하는 이유는? 안해도 됨. 그저예시였음.
     b = a.split("=") //b [id,1]
     // console.log(b)
     //왜 이건또 쿼리로 넘기지?
-    // `http://localhost:8080/users/${i}` 이렇게로도 넘길수있는거 아닌갑?? 차이가 뭐지? 그냥 쿼릴로 넘긴거임. 
+    // `http://localhost:8080/users/${i}` 이렇게로도 넘길수있는거 아닌갑?? 차이가 뭐지? 그냥 쿼릴로 넘긴거임.
     const res = await fetch(`http://localhost:8080/get?id=${b[1]}`)
-    const data = await res.json() 
+    const data = await res.json()
     user = data
     console.log(user)
-    makeList()
-    
-}
-
-//근데 이렇게 서버에 라우터를 하나 더 안만들고 원래 있는 라우터를 활용하면 안되나? 
-//이런식으로? 속도가 더 느려지거나 그런건갑 
-// let users = []
-// async function getUsers() {
-//     const a = location.search
-//     b = a.split("=")
-//     const res = await fetch("http://localhost:8080/users")
-//     const data = await res.json()
-//     users = data
-//     console.log(users[b])
-// 100만명 / 
-//     makeList()
-// }
-
-function makeList() {
-    const tbody = document.querySelector("tbody")
-    tbody.innerHTML = ""
-    tbody.innerHTML += 
-        `<tr>
-            <td><input value="${user.Name}"></td>
-            <td><input value="${user.Age}"></td>
-            <td><input value="${getGender()}"></td>
-            <td><input value="${user.Job}"></td>
-            <td><button id="edit"></button></td>
-        </tr>`
-    getGender()
-}
-
-
-
-
-
-
-function getGender() {
+    const input = document.querySelectorAll("input")
+    input[0].value = user.Name
+    input[1].value = user.Age
     if (user.Gender == 0) {
-        return "남자"
+        input[2].checked = true
     } else {
-        return "여자"
+        input[3].checked = true
     }
+    input[4].value = user.Job
 }
-
-
